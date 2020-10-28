ad4cceeb0552030580e0bcdd53c296a7b3ca2630
CatsAreMLG <joshuadgon@gmail.com>
2020-10-27 20:03:17 -0500

    Add Firebase Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FirebaseAppProvider } from 'reactfire';
 import './index.css';
 import App from './components/App';
+import firebaseConfig from './util/firebaseConfig'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
